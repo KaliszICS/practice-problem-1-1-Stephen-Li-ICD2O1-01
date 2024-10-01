6a8145e3c6be632295967615917575860a8e558e
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2024-10-01 14:12:39 +0000

    commit

--- PP1_1.py
+++ PP1_1.py
@@ -1,18 +1,17 @@
 def q1():
-  print("Hello world")
+  #Write Assignment code here
+  print("Hello World")
+
 def q2():
   #Write Assignment code here
-  print(1)
-  print(2)
-  print(3)
-  print(4)
-  print(5)
+  print("1\n2\n3\n4\n5")
+  
 def q3():
-  #Write Assignment code here
   print(" Help Me!")
-def q4():
+  
+def q4:
   #Write Assignment code here
-  print("3 x 3 = " , 3*3)
+print("3 x 3 = 9")
 
 q1()
 q2()
